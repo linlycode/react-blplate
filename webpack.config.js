0658023174ab907493c34a9c8bc7932618cd3b17
linly <linlycode@gmail.com>
2018-06-13 20:41:27 +0800

    Add webpack and config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+	entry: {
+		context: path.resolve(__dirname, 'src'),
+		app: 'app.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+	}
+}
