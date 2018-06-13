2466a49c76e8f5540a4a19366a5e826c93701d20
linly <linlycode@gmail.com>
2018-06-13 21:25:25 +0800

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,9 @@ module.exports = {
 		filename: '[name].js',
 		path: path.resolve(__dirname, 'dist'),
 	},
+	module: {
+		rules: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+		],
+	},
 }
